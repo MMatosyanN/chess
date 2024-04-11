4cec4874d9c7f36cba7e25bd20c1c55d9827e270
matos <matos@MSI>
2024-04-11 16:48:04 +0400

    Add project files.

---
+++ chess/Class2.cs
@@ -0,0 +1,38 @@
+﻿using Program.cs;
+
+namespace Program.cs;
+
+    enum Figur
+    {
+        K,//King
+        Q,//Queen
+        B,//Bishop
+        N,//Knight
+        R,//Rook
+    }
+    enum Letter
+    {
+        A = 65,
+        B = 67,
+        C = 69,
+        D = 71,
+        E = 73,
+        F = 75,
+        G = 77,
+        H = 79
+}
+
+public struct Coord
+{
+    public int X;
+    public char Y;
+    public char F;
+    public Coord(int XCoord, char YCoord, char Figur)
+    {
+        X = XCoord;
+        Y = YCoord;
+        F = Figur;
+    }
+}
+
+
---
+++ chess/Enum.cs
@@ -0,0 +1,25 @@
+﻿namespace chess
+{
+    public class Enum
+    {
+        enum Figur
+        {
+            K,//King
+            Q,//Queen
+            B,//Bishop
+            N,//Knight
+            R,//Rook
+        }
+        enum Coord
+        {
+            A=65,
+            B=67,
+            C=69,
+            D=71,
+            E=73,
+            F=75,
+            G=77,
+            H=79
+        }
+    }
+}
